37a1388ac90c262ae37559f4e41d4105d2cd16bb
camih1005 <camiloht0918@gmail.com>
2024-06-20 17:06:50 -0500

    ca

--- intro-java/src/main/java/com/intro/Main.java
+++ intro-java/src/main/java/com/intro/Main.java
@@ -1,27 +1,49 @@
 package com.intro;
 
-import java.text.MessageFormat;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        int valor = 0;
-        int acum = 0;
-        int numeroInv = 0;
-        int numeroVali = 0;
-        int[] numeros = {1,2,3,4,5};
-        int logintud = numeros.length;
 
-        try(Scanner sc = new Scanner(System.in)){
-        System.out.println(logintud);    
-            }
-
+        Main method = new Main();
+        method.concatenar();
+        method.arrayListss();
+        method.Matriz();
+        method.listadd();
+    //cxzcxcxcxcx
+    }
+    
+    ArrayList<String> agregar = new ArrayList<>();
+        
+        private  void concatenar(){
+        String inmutableStr = "Hola";
+        inmutableStr = inmutableStr.concat(" Mundo");
+        System.out.println(inmutableStr);
         }
+        private  void arrayListss(){
+            agregar.add("camilo");
+            System.out.println(agregar);
+        }
+        private  void Matriz(){
+           String [] colores = {"Amarrilo","Rojo","Verde"};
+           System.out.println(colores);
+        }
+        private  void listadd(){
+            agregar.add("carlos");
+            agregar.add(1,"christian");
+            System.out.println(agregar);
+         }
+         private  void gets(){
+            
+            agregar.add(1,"christian");
+            System.out.println(agregar);
+         }
+        
+}
+    
      
 
 
-}
+
 
